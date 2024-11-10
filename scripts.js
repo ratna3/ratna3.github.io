e712aec44cda89eb350e295a6970c6907a563de4
Ratna Kirti <102272008+ratna3@users.noreply.github.com>
2024-11-09 23:41:59 -0600

    added new font and button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-    
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-    
-    // You can handle form submission here (e.g., send data to a server)
-    alert(`Thank you, ${name}! Your message has been sent.`);
-    
-    // Optionally, reset the form
-    document.getElementById('contact-form').reset();
-  });
+  event.preventDefault(); // Prevent form submission
   
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const description = document.getElementById('description').value;
+  
+  // Handle form submission here (e.g., send data to a server)
+  alert(`Thank you, ${name}! Your message has been sent.`);
+  
+  // Optionally, reset the form
+  document.getElementById('contact-form').reset();
+});
